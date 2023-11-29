a40e701eb342a7b5a69b0749f4c28324bddc7cbf
mbsandur11 <62284838+mbsandur11@users.noreply.github.com>
2023-11-29 11:06:23 +0530

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-  ami             = "ami-0230bd60aa48260c6"  
-  instance_type   = "t2.micro"
-  key_name        = "techverito_key"  
- vpc_security_group_ids = [aws_security_group.instance_sg.id]  
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -10,31 +10,39 @@ resource "aws_instance" "example" {
     sudo yum install -y docker
     sudo service docker start
     sudo usermod -a -G docker ec2-user
-    sudo curl -L "https://github.com/docker/compose/releases/download/<DOCKER_COMPOSE_VERSION>/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/${var.docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo yum install -y aws-cli
     EOF
 
- tags = {
+  tags = {
     Name = "techverito_test"
+  }
+  
+  lifecycle {
+    ignore_changes = [tags]  # Ignore changes to tags after creation
   }
 }
 
-resource "aws_security_group" "instance_sg" {  # Changed name to avoid confusion
+resource "aws_security_group" "instance_sg" {
   name_prefix = "example-instance-sg"
-  vpc_id      = "vpc-0ead1ccc5da2db328"  
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.security_group_ingress_port
+    to_port     = var.security_group_ingress_port
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.security_group_cidr_block]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.security_group_ingress_port
+    to_port     = var.security_group_ingress_port
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.security_group_cidr_block]
   }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
