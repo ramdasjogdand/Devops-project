2b2fa8861580b26caff37b86b6798a3563b0d4aa
ramdasjogdand <135848804+ramdasjogdand@users.noreply.github.com>
2024-06-24 21:26:38 +0530

    Delete terraform/aws directory

--- terraform/aws/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecrpublic_repository" "techverito_backend" {
-  repository_name = var.ecr_repository_backend
-}
-
-resource "aws_ecrpublic_repository" "techverito_frontend" {
-  repository_name = var.ecr_repository_frontend
-}
